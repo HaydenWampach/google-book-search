ba414876eab6567da803d891861bcefaf354269d
Hayden Wampach <wampachhayden@gmail.com>
2020-01-27 21:46:08 -0800

    added jumbotron function for page

---
+++ my-app/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div>
+      <div className="jumbotron text-center">
+        <h2>Looking for a good book? Enter the book title below to find out a bit more about it!</h2>
+        <h4>Click 'Save' to add a book to your favorites</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
